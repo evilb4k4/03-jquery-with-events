9ce9db1daa1417130a4df4d78ecec3f853a69932
RyleeAndrews <ryleeandrews73@gmail.com>
2017-05-10 13:45:44 -0700

    finished first to-do

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -40,6 +40,9 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      var $articleBody = $('article-body');
+      $articleBody.hide();
+
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
